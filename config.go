2649aef47b7942ef7f2885cfec5f70098fc3f4d2
Julien Viard de Galbert <julien@vdg.name>
2024-02-03 10:11:08 +0100

    feat: extend ssh_config to handle multiple IdentityFile

--- config.go
+++ config.go
@@ -3,12 +3,14 @@ package ssh_client
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/kevinburke/ssh_config"
 )
 
 // Note: this should really be part of ssh_config,
 // TODO add lazy open similar to UserSettings
+// Oh oh! Done on git but not released.
 
 type Settings struct {
 	cfg *ssh_config.Config
@@ -53,9 +55,59 @@ func (s *Settings) GetAllStrict(alias, key string) ([]string, error) {
 	if err != nil || val != nil {
 		return val, err
 	}
-	// TODO: IdentityFile has multiple default values that we should return.
+	if def := MultipleDefault(key); def != nil {
+		return def, nil
+	}
+	return []string{}, nil
+}
+
+// FROM ssh_config/validators.go
+// these identities are used for SSH protocol 2
+var defaultProtocol2Identities = []string{
+	"~/.ssh/id_dsa",
+	"~/.ssh/id_ecdsa",
+	"~/.ssh/id_ed25519",
+	"~/.ssh/id_rsa",
+}
+
+// MultipleDefault returns the default values for the given keyword, for example// {"22"} if the keyword is "Port". MultipleDefault returns nil if the keyword
+// has no default, or if the keyword is unknown. Keyword matching is
+// case-insensitive.
+//
+// MultipleDefault return more than one element for "IdentityFile" keyword.
+func MultipleDefault(key string) []string {
 	if def := ssh_config.Default(key); def != "" {
-		return []string{def}, nil
+		if strings.ToLower(key) == strings.ToLower("IdentityFile") {
+			return append([]string{def}, defaultProtocol2Identities...)
+		}
+		return []string{def}
+	}
+	return nil
+}
+
+// UserSettings overrides ssh_config.UserSettings to handle MultipleDefault
+type UserSettings struct {
+	*ssh_config.UserSettings
+}
+
+var DefaultUserSettings = &UserSettings{ssh_config.DefaultUserSettings}
+
+func (u *UserSettings) GetAll(alias, key string) []string {
+	val, _ := u.GetAllStrict(alias, key)
+	return val
+}
+
+func (u *UserSettings) GetAllStrict(alias, key string) ([]string, error) {
+	val, err := u.UserSettings.GetAllStrict(alias, key)
+	if strings.ToLower(key) == strings.ToLower("IdentityFile") && len(val) == 1 {
+		// TODO should discriminate explicit values from default ones to not fail on trying to open those files if not found.
+		// Ugly hack, could be wrong if alias has a single "~/.ssh/identity"
+		if val[0] == "~/.ssh/identity" {
+			return MultipleDefault(key), nil
+		}
+	}
+	if err != nil || val != nil {
+		return val, err
 	}
 	return []string{}, nil
 }
